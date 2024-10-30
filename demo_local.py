09404986808f427d3eaede0814ae4275c353427d
Alexander Kurz <alex.kurz95@gmail.com>
2024-10-30 14:15:12 +0100

    Load data from single parquet file

--- demo_local.py
+++ demo_local.py
@@ -6,17 +6,25 @@ from PIL import Image
 from renumics import spotlight
 
 
-def main(data_p: Path = Path.home() / "Data" / "TrainDatasets" / "casia_webface-parquet"):
+def main(
+    data_p: Path = Path.home() / "Data" / "TrainDatasets" / "parquet-files" / "ms1mv2.parquet",
+):
     assert data_p.exists()
-    ds = datasets.load_dataset("parquet", data_dir=data_p, split="train")
+    # ds = datasets.load_dataset("parquet", data_files=str(data_p), split="train")
+    ds = datasets.Dataset.from_parquet(str(data_p), split="train")
     print("Number of items:", len(ds))
 
-    # img, label = ds[0].values()
+    entry = ds[0]
+    print("Keys:", entry.keys())
+    img = entry["img"]
+    label = entry["label"]
+    print("Image properties:", img)
+
     # plt.imshow(img)
     # plt.xlabel(label)
     # plt.show()
 
-    spotlight.show(ds)
+    # spotlight.show(ds)
 
 
 if __name__ == "__main__":
