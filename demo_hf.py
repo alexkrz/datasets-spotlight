cbab2a263c0b7417e1f40c8b65973ba1385b3d66
Alexander Kurz <alex.kurz95@gmail.com>
2024-10-31 13:08:35 +0100

    Save huggingface dataset as single .parquet file

--- demo_hf.py
+++ demo_hf.py
@@ -1,15 +1,34 @@
+import json
+from pathlib import Path
+
 import datasets
 import pandas as pd
 from renumics import spotlight
 
 
-def main():
+def main(out_dir: Path = Path.home() / "Data" / "EmotionRecognition" / "affectnethq"):
     # df = pd.read_csv("https://renumics.com/data/mnist/mnist-tiny.csv")
     # print(df.head())
     # spotlight.show(df, dtype={"image": spotlight.Image})
 
-    ds = datasets.load_dataset("cifar10", split="test")
-    spotlight.show(ds)
+    ds = datasets.load_dataset("Piro17/affectnethq", split="train")
+
+    # CORRECTION: It looks like the labels are already included in the .parquet file.
+    # OLD: Unfortunately, currently the label mapping is not included in the parquet file.
+    # Instead, we should export the labels as a separate .json file
+    print(ds.features)
+    # class_label: datasets.ClassLabel = ds.features["label"]
+    # class_names = class_label.names
+    # class_dict = {idx: name for idx, name in enumerate(class_names)}
+
+    # with open(out_dir / "class_names.json", "w") as f:
+    #     json.dump(class_dict, f, indent=4)
+    #     f.write("\n")  # Add a trailing newline
+
+    # Here we save the dataset as a single .parquet file
+    ds.to_parquet(out_dir / "affectnethq.parquet")
+
+    # spotlight.show(ds)
 
 
 if __name__ == "__main__":
--- demo_local.py
+++ demo_local.py
@@ -7,24 +7,26 @@ from renumics import spotlight
 
 
 def main(
-    data_p: Path = Path.home() / "Data" / "TrainDatasets" / "parquet-files" / "ms1mv2.parquet",
+    data_p: Path = Path.home() / "Data" / "EmotionRecognition" / "affectnethq" / "affectnethq.parquet",  # fmt: skip
 ):
     assert data_p.exists()
     # ds = datasets.load_dataset("parquet", data_files=str(data_p), split="train")
     ds = datasets.Dataset.from_parquet(str(data_p), split="train")
+    print(ds.features)
     print("Number of items:", len(ds))
 
     entry = ds[0]
     print("Keys:", entry.keys())
-    img = entry["img"]
+    img = entry["image"]
     label = entry["label"]
     print("Image properties:", img)
+    print("Label:", label)
 
     # plt.imshow(img)
     # plt.xlabel(label)
     # plt.show()
 
-    # spotlight.show(ds)
+    spotlight.show(ds)
 
 
 if __name__ == "__main__":
