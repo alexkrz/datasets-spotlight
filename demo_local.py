231622ac4f7f89d0eb44d8300f220e971ceecf39
Alexander Kurz <alex.kurz95@gmail.com>
2025-08-20 12:41:37 +0200

    Visualize b3fd.parquet dataset

--- demo_local.py
+++ demo_local.py
@@ -1,4 +1,5 @@
 import json
+import os
 from pathlib import Path
 
 import datasets
@@ -10,17 +11,18 @@ CAST_COLUMN = False
 
 
 def main(
-    data_p: Path = Path.home() / "Data" / "FairnessBias" / "bupt_balance-parquet" / "bupt_balance.parquet",  # fmt: skip
+    data_p: str = os.environ["HOME"] + "/Data/Experimental/B3FD/B3FD_parquet/b3fd.parquet",
 ):
+    data_p = Path(data_p)  # type: Path
     assert data_p.exists()
     # ds = datasets.load_dataset("parquet", data_files=str(data_p), split="train")
-    ds = datasets.Dataset.from_parquet(str(data_p), split="train")
+    ds = datasets.Dataset.from_parquet(str(data_p))
     print("Number of items:", len(ds))
     print(ds.features)
 
     if CAST_COLUMN and data_p.stem == "bupt_balance":
         # Add ethnicity names as ClassLabel
-        with open(data_p.parent / "ethnicity_labels.json", "r") as f:
+        with open(data_p.parent / "ethnicity_labels.json") as f:
             ethnicity_labels = json.load(f)
         ethnicity_names = list(ethnicity_labels.values())
         ds = ds.cast_column("ethnicity", datasets.ClassLabel(names=ethnicity_names))
